686d678611d44ec50deb4ed2dd4f29f4f0bf7fb2
Bennie Mosher <benniemosher@gmail.com>
2025-03-25 06:59:14 -0600

    feat: Changed variables names in zoom service

--- applications/hello_world_app/app/services/zoom_api.rb
+++ applications/hello_world_app/app/services/zoom_api.rb
@@ -8,24 +8,23 @@ module ZoomApi
   ZOOM_TOKEN_URL = "https://zoom.us/oauth/token"
   ZOOM_USER_INFO_URL = "https://api.zoom.us/v2/users/me"
 
-  # TODO: Change variable names to include zoom
-  CLIENT_ID = ENV["ZOOM_CLIENT_ID"]
-  CLIENT_SECRET = ENV["ZOOM_CLIENT_SECRET"]
-  REDIRECT_URI = ENV["ZOOM_REDIRECT_URI"]
+  ZOOM_CLIENT_ID = ENV["ZOOM_CLIENT_ID"]
+  ZOOM_CLIENT_SECRET = ENV["ZOOM_CLIENT_SECRET"]
+  ZOOM_REDIRECT_URI = ENV["ZOOM_REDIRECT_URI"]
 
   def self.authorization_url
-    "#{ZOOM_AUTHORIZE_URL}?response_type=code&client_id=#{CLIENT_ID}&redirect_uri=#{REDIRECT_URI}"
+    "#{ZOOM_AUTHORIZE_URL}?response_type=code&client_id=#{ZOOM_CLIENT_ID}&redirect_uri=#{ZOOM_REDIRECT_URI}"
   end
 
   def self.exchange_code_for_token(code)
     uri = URI.parse(ZOOM_TOKEN_URL)
     request = Net::HTTP::Post.new(uri)
-    request["Authorization"] = "Basic #{Base64.strict_encode64("#{CLIENT_ID}:#{CLIENT_SECRET}")}"
+    request["Authorization"] = "Basic #{Base64.strict_encode64("#{ZOOM_CLIENT_ID}:#{ZOOM_CLIENT_SECRET}")}"
     request["Content-Type"] = "application/x-www-form-urlencoded"
     request.set_form_data({
       grant_type: "authorization_code",
       code: code,
-      redirect_uri: REDIRECT_URI
+      redirect_uri: ZOOM_REDIRECT_URI
     })
 
     response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
