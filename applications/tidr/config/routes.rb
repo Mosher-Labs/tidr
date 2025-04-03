0257acb7bbdb167ca38f9d8995a00e9efa7ff955
Bennie Mosher <benniemosher@gmail.com>
2025-04-03 11:48:50 -0600

    feat!: Renamed application to Tidr

--- applications/tidr/config/routes.rb
+++ applications/tidr/config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users
 
-  get "hello_world/index"
-
   get "up" => "rails/health#show", as: :rails_health_check
 
   get "/zoom/connect", to: "zoom#connect"
